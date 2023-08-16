0e7178a47f5c72a3990d5defa7afa7defd093d9b
irazramin <irazramin@gmail.com>
2023-08-16 22:42:08 +0600

    Update application port

--- src/app/http/kernel.ts
+++ src/app/http/kernel.ts
@@ -1,5 +1,6 @@
 import express from 'express'
-import * as http from "http";
+import {config} from "../utils";
+
 export default class Kernel {
     protected app: express.Application;
 
@@ -7,13 +8,13 @@ export default class Kernel {
         this.app = express();
     }
 
-    listen(port: number | string = 4000): void {
+    listen(port: number | string = 3000): void {
         this.app.listen(port, (): void => {
             console.log(`Server is listen at ${port}`);
         });
     }
 
     getPort(): number | string {
-        return 4000;
+        return config('app.port');
     }
 }
